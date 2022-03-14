c105807d145d0703854a8377ba0c871e8da4f9ef
Ryan Truong <ryan.truong2304@gmail.com>
2022-03-14 17:50:55 +1100

    Got the calculator workinggit add .!

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-let display = document.querySelector('.display');
+const divideButton = document.querySelector('.divide');
+const multiplyButton = document.querySelector('.multiply');
+const plusButton = document.querySelector('.plus');
+const minusButton = document.querySelector('.minus');
 
 const buttonNumber = document.querySelectorAll('.number');
 const buttonOperator = document.querySelectorAll('.operator');
@@ -10,8 +13,10 @@ const operatorButtons = document.querySelectorAll('[data-operator]');
 const equalsButton = document.querySelector('[data-equals]');
 const clearButton = document.querySelector('[data-clear]');
 
-let operand = document.querySelector('[data-operand]');
-let operandBefore = 0;
+let operand = document.querySelector('[data-operand]').innerHTML;
+let operandBefore;
+
+let operatorGlobal = undefined;
 
 
 //Changes the colour when the buttons are clicked
@@ -26,7 +31,24 @@ buttonNumber.forEach(button => {
 
 buttonOperator.forEach(button => {
     button.addEventListener('click', () => {
-        button.classList.toggle('click-operator');
+        button.classList.add('click-operator');
+        if(button.innerHTML === '+') {
+            divideButton.classList.remove('click-operator');
+            multiplyButton.classList.remove('click-operator');
+            minusButton.classList.remove('click-operator');
+        } else if(button.innerHTML === '-') {
+            divideButton.classList.remove('click-operator');
+            multiplyButton.classList.remove('click-operator');
+            plusButton.classList.remove('click-operator');
+        } else if(button.innerHTML === '/') {
+            minusButton.classList.remove('click-operator');
+            multiplyButton.classList.remove('click-operator');
+            plusButton.classList.remove('click-operator');
+        } else if(button.innerHTML === '*') {
+            minusButton.classList.remove('click-operator');
+            divideButton.classList.remove('click-operator');
+            plusButton.classList.remove('click-operator');
+        }
     });
 });
 
@@ -44,34 +66,16 @@ buttonClear.addEventListener('click', () => {
     }, 0400);
 });
 
-//operator functions
-function add(num1, num2) {
-    let sum = num1 + num2;
-    return sum;
-}
 
-function subtract(num1, num2) {
-    let difference = num1 - num2;
-    return difference;
-}
-
-function divide(num1, num2) {
-    if(num1 === 0 || num2 === 0) {
-        return 'C\'mon really?!';
-    } else {
-        let quotient = num1 / num2;
-        return quotient;
-    }
-}
-
-function multiply(num1, num2) {
-    let product = num1 * num2;
-    return product;
-}
-
+//calculator functions
 function allClear() {
     operand = '';
     operandBefore = '';
+    buttonOperator.forEach(button => {
+        button.classList.remove('click-operator');
+    });
+    operatorGlobal = undefined;
+    updateDisplay();
 }
 
 function deleteNum() {
@@ -79,16 +83,53 @@ function deleteNum() {
 }
 
 function appendNumber(number) {
+    if(number === '.' && operand.includes('.')) return;
     operand = operand.toString() + number.toString();
-    console.log(operand)
 }
 
 function chooseOperator(operator) {
-
+    operatorGlobal = operator;
+    if(operand === '') return;
+    if(operandBefore !== '') {
+        operate(operatorGlobal);
+    }
+    operandBefore = operand;
+    operand = '';
 }
 
-function operate() {
+function operate(operator) {
+    let result;
+    const previous = parseFloat(operandBefore);
+    const current = parseFloat(operand);
+    if(isNaN(previous) || isNaN(current)) return;
+    switch(operator) {
+        case '+': 
+            result = previous + current;
+            break;
+        case '-':
+            result = previous - current;
+            break;
+        case '/':
+            if(previous === 0 || current === 0) {
+                result = 'C\'mon Really?!'
+                break;
+            } else {
+                result = previous / current;
+                break;
+            }
+        case '*':
+            result = previous * current;
+            break;
+        default:
+            return;
+    }
+    operand = result;
+    operatorGlobal = undefined;
+    operandBefore = '';
 
+    buttonOperator.forEach(button => {
+        button.classList.remove('click-operator');
+    });
 }
 
 function updateDisplay() {
@@ -98,6 +139,22 @@ function updateDisplay() {
 
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
-        appendNumber(button.innerHTML);
+        appendNumber(button.textContent);
+        updateDisplay();
     });
+});
+
+operatorButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        chooseOperator(button.textContent);
+    });
+});
+
+clearButton.addEventListener('click', () => {
+    allClear();
+});
+
+equalsButton.addEventListener('click', () => {
+    operate(operatorGlobal);
+    updateDisplay();
 });
